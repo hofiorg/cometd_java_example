4a57fc7d650d52da8198f6f11727a5243554482e
Christian Hofmann <hofiorg@googlemail.com>
2018-05-22 10:38:17 +0200

    Initial commit

---
+++ src/main/java/org/hofi/cometd/CometDDemo.java
@@ -0,0 +1,106 @@
+package org.hofi.cometd;
+
+import org.cometd.annotation.AnnotationCometDServlet;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ServerChannel;
+import org.cometd.bayeux.server.ServerMessage.Mutable;
+import org.cometd.bayeux.server.ServerSession;
+import org.cometd.server.DefaultSecurityPolicy;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.resource.ResourceCollection;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
+/**
+ * Main class for cometd demo.
+ */
+public class CometDDemo {
+  public static void main(String[] args) throws Exception {
+    int port = args.length == 0 ? 8080 : Integer.parseInt(args[0]);
+
+    QueuedThreadPool qtp = new QueuedThreadPool();
+    qtp.setMinThreads(5);
+    qtp.setMaxThreads(200);
+    Server server = new Server(qtp);
+
+    ServerConnector connector = new ServerConnector(server);
+    connector.setPort(port);
+    connector.setIdleTimeout(120000);
+    connector.setAcceptQueueSize(5000);
+    server.addConnector(connector);
+
+    SslContextFactory sslContextFactory = new SslContextFactory();
+    sslContextFactory.setKeyStorePath("src/main/resources/keystore.jks");
+    sslContextFactory.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
+    sslContextFactory.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
+    sslContextFactory.setTrustStorePath("src/main/resources/keystore.jks");
+    sslContextFactory.setTrustStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
+    ServerConnector sslConnector = new ServerConnector(server, sslContextFactory);
+    sslConnector.setPort(port - 80 + 443);
+    server.addConnector(connector);
+
+    ContextHandlerCollection contexts = new ContextHandlerCollection();
+    server.setHandler(contexts);
+
+    ServletContextHandler context = new ServletContextHandler(contexts, "/", ServletContextHandler.SESSIONS);
+    context.setBaseResource(new ResourceCollection(
+      Resource.newResource("../../cometd-demo/src/main/webapp/"),
+      Resource.newResource("../../cometd-javascript/common/src/main/webapp/"),
+      Resource.newResource("../../cometd-javascript/jquery/src/main/webapp/"),
+      Resource.newResource("../../cometd-javascript/examples-jquery/src/main/webapp/"),
+      Resource.newResource("../../cometd-javascript/dojo/src/main/webapp/"),
+      Resource.newResource("../../cometd-javascript/examples-dojo/src/main/webapp/"),
+      Resource.newResource("../../cometd-demo/target/war/work/org.cometd.javascript/cometd-javascript-dojo/"),
+      Resource.newResource("../../cometd-demo/target/war/work/org.cometd.javascript/cometd-javascript-jquery/"))
+    );
+
+    ServletHolder dftServlet = context.addServlet(DefaultServlet.class, "/");
+    dftServlet.setInitOrder(1);
+
+    // CometD servlet
+    AnnotationCometDServlet cometdServlet = new AnnotationCometDServlet();
+    ServletHolder comet = new ServletHolder(cometdServlet);
+    context.addServlet(comet, "/cometd/*");
+    comet.setInitParameter("timeout", "20000");
+    comet.setInitParameter("interval", "100");
+    comet.setInitParameter("maxInterval", "10000");
+    comet.setInitParameter("multiSessionInterval", "5000");
+    comet.setInitParameter("services", "org.cometd.examples.ChatService");
+    comet.setInitOrder(2);
+
+    ServletHolder demo = context.addServlet(CometDDemoServlet.class, "/demo");
+    demo.setInitOrder(3);
+
+    server.start();
+
+    BayeuxServer bayeux = cometdServlet.getBayeux();
+    bayeux.setSecurityPolicy(new DefaultSecurityPolicy());
+
+    // Demo lazy messages
+    if (Boolean.getBoolean("LAZY")) {
+      bayeux.addExtension(new BayeuxServer.Extension.Adapter() {
+        @Override
+        public boolean rcv(ServerSession from, Mutable message) {
+          if (message.getChannel().startsWith("/chat/") && !message.isPublishReply() && message.getData().toString().contains("lazy")) {
+            message.setLazy(true);
+          }
+          return true;
+        }
+      });
+    }
+
+    // Demo lazy messages
+    if (Boolean.getBoolean("LAZYCHAT")) {
+      String channelName = "/chat/demo";
+      final ServerChannel chat_demo = bayeux.createChannelIfAbsent(channelName).getReference();
+      chat_demo.setLazy(true);
+      chat_demo.setPersistent(true);
+    }
+  }
+}
---
+++ src/main/java/org/hofi/cometd/CometDDemoServlet.java
@@ -0,0 +1,114 @@
+package org.hofi.cometd;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+
+import org.cometd.annotation.Configure;
+import org.cometd.annotation.Listener;
+import org.cometd.annotation.RemoteCall;
+import org.cometd.annotation.ServerAnnotationProcessor;
+import org.cometd.annotation.Service;
+import org.cometd.bayeux.Message;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ConfigurableServerChannel;
+import org.cometd.bayeux.server.ServerChannel;
+import org.cometd.bayeux.server.ServerMessage;
+import org.cometd.bayeux.server.ServerSession;
+import org.cometd.server.BayeuxServerImpl;
+import org.cometd.server.authorizer.GrantAuthorizer;
+import org.cometd.server.ext.AcknowledgedMessagesExtension;
+import org.cometd.server.ext.TimesyncExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CometDDemoServlet extends HttpServlet {
+  private static final Logger logger = LoggerFactory.getLogger(CometDDemoServlet.class);
+
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    final BayeuxServerImpl bayeux = (BayeuxServerImpl)getServletContext().getAttribute(BayeuxServer.ATTRIBUTE);
+
+    if (bayeux == null) {
+      throw new UnavailableException("No BayeuxServer!");
+    }
+
+    // Create extensions
+    bayeux.addExtension(new TimesyncExtension());
+    bayeux.addExtension(new AcknowledgedMessagesExtension());
+
+    // Deny unless granted
+
+    bayeux.createChannelIfAbsent("/**", new ServerChannel.Initializer() {
+      @Override
+      public void configureChannel(ConfigurableServerChannel channel) {
+        channel.addAuthorizer(GrantAuthorizer.GRANT_NONE);
+      }
+    });
+
+    // Allow anybody to handshake
+    bayeux.getChannel(ServerChannel.META_HANDSHAKE).addAuthorizer(GrantAuthorizer.GRANT_PUBLISH);
+
+    ServerAnnotationProcessor processor = new ServerAnnotationProcessor(bayeux);
+    processor.process(new EchoRPC());
+    processor.process(new Monitor());
+    // processor.process(new ChatService());
+
+    bayeux.createChannelIfAbsent("/foo/bar/baz", new ConfigurableServerChannel.Initializer.Persistent());
+
+    if (logger.isDebugEnabled()) {
+      logger.debug(bayeux.dump());
+    }
+  }
+
+  @Override
+  public void destroy() {
+    super.destroy();
+  }
+
+  @Service("echo")
+  public static class EchoRPC {
+    @Configure("/service/echo")
+    private void configureEcho(ConfigurableServerChannel channel) {
+      channel.addAuthorizer(GrantAuthorizer.GRANT_SUBSCRIBE_PUBLISH);
+    }
+
+    @RemoteCall("echo")
+    public void doEcho(RemoteCall.Caller caller, Map<String, Object> data) {
+      logger.info("ECHO from " + caller.getServerSession() + ": " + data);
+      caller.result(data);
+    }
+  }
+
+  @Service("monitor")
+  public static class Monitor {
+    @Listener("/meta/subscribe")
+    public void monitorSubscribe(ServerSession session, ServerMessage message) {
+      logger.info("Monitored Subscribe from " + session + " for " + message.get(Message.SUBSCRIPTION_FIELD));
+    }
+
+    @Listener("/meta/unsubscribe")
+    public void monitorUnsubscribe(ServerSession session, ServerMessage message) {
+      logger.info("Monitored Unsubscribe from " + session + " for " + message.get(Message.SUBSCRIPTION_FIELD));
+    }
+
+    @Listener("/meta/*")
+    public void monitorMeta(ServerSession session, ServerMessage message) {
+      if (logger.isDebugEnabled()) {
+        logger.debug(message.toString());
+      }
+    }
+  }
+
+  @Override
+  public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    ((HttpServletResponse)res).sendError(503);
+  }
+}
